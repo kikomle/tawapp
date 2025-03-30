834d065bdc96e15fa11058cf3002aed53dda60e2
enzokutic <kutic.enzo@gmail.com>
2025-03-30 19:40:46 +0200

    initial commit

---
+++ src/main/java/enzo/tmapp/tawapp/TawappApplication.java
@@ -0,0 +1,13 @@
+package enzo.tmapp.tawapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TawappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TawappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/enzo/tmapp/tawapp/TawappApplicationTests.java
@@ -0,0 +1,13 @@
+package enzo.tmapp.tawapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TawappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
